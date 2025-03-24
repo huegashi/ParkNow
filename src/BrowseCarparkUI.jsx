2aadfb8271a93c7cfa76b9a55c24f1265f4c6deb
xychen2001 <ch0032yu@e.ntu.edu.sg>
2025-03-24 16:44:53 +0800

    <Changed background colors for homepage>

--- src/BrowseCarparkUI.jsx
+++ src/BrowseCarparkUI.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const BrowseCarparkUI = ({ searchTerm, handleInputChange, handleKeyPress, filteredOptions, handleChange, showTable }) => {
+const BrowseCarparkUI = ({
+  searchTerm,
+  handleInputChange,
+  handleKeyPress,
+  filteredOptions,
+  handleChange,
+  showTable,
+}) => {
   return (
-    <div className="browse-carpark" style={{ maxWidth: '400px', margin: '0 auto', textAlign: 'center' }}>
-      <div style={{ color: 'white', marginBottom: '5px', fontSize: '0.8em' }}>
+    <div
+      className="browse-carpark"
+      style={{ maxWidth: "400px", margin: "0 auto", textAlign: "center" }}
+    >
+      <div style={{ color: "white", marginBottom: "5px", fontSize: "0.8em" }}>
         *Press Enter to show all relevant carparks
       </div>
       <input
@@ -12,20 +22,28 @@ const BrowseCarparkUI = ({ searchTerm, handleInputChange, handleKeyPress, filter
         onChange={handleInputChange}
         onKeyPress={handleKeyPress}
         placeholder="Search for a carpark..."
-        style={{ width: '100%', padding: '8px', marginBottom: '10px' }}
+        style={{
+          width: "100%",
+          padding: "8px",
+          marginBottom: "10px",
+          backgroundColor: "white",
+        }}
       />
       {showTable && filteredOptions.length > 0 && (
-        <table className="filtered-options-table" style={{ margin: '0 auto' }}>
+        <table
+          className="filtered-options-table"
+          style={{ margin: "0 auto", backgroundColor: "white" }}
+        >
           <thead>
             <tr>
-              <th style={{ paddingRight: '20px' }}>Carpark</th>
+              <th style={{ paddingRight: "20px" }}>Carpark</th>
               <th>Select</th>
             </tr>
           </thead>
           <tbody>
-            {filteredOptions.map(option => (
+            {filteredOptions.map((option) => (
               <tr key={option.value}>
-                <td style={{ paddingRight: '20px' }}>{option.label}</td>
+                <td style={{ paddingRight: "20px" }}>{option.label}</td>
                 <td>
                   <button onClick={() => handleChange(option)}>Select</button>
                 </td>
