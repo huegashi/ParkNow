f44a18faa3ee668fabd47b01db250d3087b476e4
Jxredkong <jaredlovesbutter@gmail.com>
2025-04-08 15:28:24 +0800

    centralise check in and checkout ui display box

--- src/views/pages/CheckOutUI.jsx
+++ src/views/pages/CheckOutUI.jsx
@@ -2,14 +2,16 @@ import "../../styles/Check.css";
 
 function CheckOutUI({ booking, error, handleCheckOut }) {
   return (
-    <div className="checkin-container">
-      <h2>Check-Out</h2>
-      {error && <p className="error">{error}</p>}
-      <p><strong>Carpark:</strong> {booking?.carpark || "Loading..."}</p>
-      <p><strong>Status:</strong> {booking?.status || "Loading..."}</p>
-      <button onClick={handleCheckOut} disabled={!booking}>
-        Check Out
-      </button>
+    <div className="parent-container">
+      <div className="checkin-container">
+        <h2>Check-Out</h2>
+        {error && <p className="error">{error}</p>}
+        <p><strong>Carpark:</strong> {booking?.carpark || "Loading..."}</p>
+        <p><strong>Status:</strong> {booking?.status || "Loading..."}</p>
+        <button onClick={handleCheckOut} disabled={!booking}>
+          Check Out
+        </button>
+      </div>
     </div>
   );
 }
